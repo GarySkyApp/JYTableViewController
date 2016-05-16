4037b7fc1cb80605f2fc74271d96ddbb5a6548b6
JiangHeguang <JHG_GarySky@163.com>
2016-05-16 11:57:49 +0800

    1.0.0
    1、tableView的delegate委托类
    2、为一些功能约定好框架
    3、使用了FDT+empty

---
+++ JYTableViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYTableViewController
+//
+//  Created by Gary on 16/5/16.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JYTableViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JYTableViewController
+//
+//  Created by Gary on 16/5/16.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
